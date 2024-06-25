77cf95ec5b6abece4349c74355ea022beb998487
Kirill Gorbunov <4pduck@gmail.com>
2024-06-25 03:32:10 +0300

    Initial commit

---
+++ Homework STL 1.2/Homework STL 1.2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::set<int> unique_numbers;
+    for (int i = 0; i < n; ++i) {
+        int num;
+        std::cin >> num;
+        unique_numbers.insert(num);
+    }
+
+    std::vector<int> sorted_unique_numbers(unique_numbers.begin(), unique_numbers.end());
+
+    std::sort(sorted_unique_numbers.rbegin(), sorted_unique_numbers.rend());
+
+    for (int num : sorted_unique_numbers) {
+        std::cout << num << std::endl;
+    }
+
+    return 0;
+}
