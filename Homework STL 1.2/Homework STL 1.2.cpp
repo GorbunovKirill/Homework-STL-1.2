5e7fd2b56aad0e90e2b040e8f7f12fa252506aa2
Kirill Gorbunov <4pduck@gmail.com>
2024-06-25 16:36:52 +0300

    fixed commit

--- Homework STL 1.2/Homework STL 1.2.cpp
+++ Homework STL 1.2/Homework STL 1.2.cpp
@@ -2,25 +2,27 @@
 #include <set>
 #include <vector>
 #include <algorithm>
+#include <iterator>
 
 int main() {
+    std::cout << "[IN]:\n";
     int n;
     std::cin >> n;
 
     std::set<int> unique_numbers;
-    for (int i = 0; i < n; ++i) {
-        int num;
-        std::cin >> num;
-        unique_numbers.insert(num);
-    }
+
+ 
+    std::copy_n(std::istream_iterator<int>(std::cin), n, std::inserter(unique_numbers, unique_numbers.end()));
+
 
     std::vector<int> sorted_unique_numbers(unique_numbers.begin(), unique_numbers.end());
 
     std::sort(sorted_unique_numbers.rbegin(), sorted_unique_numbers.rend());
 
-    for (int num : sorted_unique_numbers) {
+    std::cout << "[OUT]:\n";
+    std::for_each(sorted_unique_numbers.begin(), sorted_unique_numbers.end(), [](int num) {
         std::cout << num << std::endl;
-    }
+        });
 
     return 0;
 }
